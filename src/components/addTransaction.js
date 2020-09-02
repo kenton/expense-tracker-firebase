6402eddc7536bee56fb0c386087369b06bdb3d0e
Kenton Newby <kentonnewby@gmail.com>
2020-09-02 17:08:42 -0400

    Refactors addTransaction

--- src/components/addTransaction.js
+++ src/components/addTransaction.js
@@ -1,36 +1,29 @@
-import React, {useState, useContext} from 'react';
-import { AppContext } from '../app';
-import { firestore } from '../firebase';
-import { collectIdsAndDocs } from '../utils';
+import React, { useState } from "react";
+import { firestore } from "../firebase";
 
 export const AddTransaction = () => {
   const [description, setDescription] = useState("");
   const [amount, setAmount] = useState(0);
-  const { transactions, setTransactions } = useContext(AppContext);
 
   // FIXME: Consider renaming this as it's confusing since this component has the same name
-  const addTransaction = async transaction => {
-    const docRef = await firestore.collection('transactions').add(transaction);
-    const doc = await docRef.get();
-    const newTransaction = collectIdsAndDocs(doc);
-
-    setTransactions([newTransaction, ...transactions]);
+  const addTransaction = async (transaction) => {
+    await firestore.collection("transactions").add(transaction);
 
     // TODO: Can't this part be done as part of a useEffect?
     setDescription("");
     setAmount(0);
-  }
+  };
 
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
     const newTransaction = {
       description,
-      amount: +amount
-    }
+      amount: +amount,
+    };
 
     addTransaction(newTransaction);
-  }
+  };
 
   return (
     <>
@@ -38,17 +31,32 @@ export const AddTransaction = () => {
       <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="description">Text</label>
-          <input type="description" value={description} onChange={(e) => {setDescription(e.target.value)}} id="description" placeholder="Enter description..." />
+          <input
+            type="description"
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+            id="description"
+            placeholder="Enter description..."
+          />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" value={amount} onChange={(e) => {setAmount(e.target.value)}} placeholder="Enter amount..." />
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => {
+              setAmount(e.target.value);
+            }}
+            placeholder="Enter amount..."
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
     </>
-  )
-}
+  );
+};
