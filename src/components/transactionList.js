ac8544cb2cc64ff2b808ebe744e5dc9d3d7ce727
Kenton Newby <kentonnewby@gmail.com>
2020-09-09 22:10:35 -0400

    Misc cleanup

--- src/components/transactionList.js
+++ src/components/transactionList.js
@@ -1,17 +1,18 @@
-import React, { useContext } from 'react';
-import { Transaction } from './transaction';
-import { AppContext } from '../app';
+import React, { useContext } from "react";
+import { Transaction } from "./transaction";
+import { AppContext } from "../app";
 
 export const TransactionList = () => {
-  const {transactions} = useContext(AppContext);
+  const { transactions } = useContext(AppContext);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        { transactions.map((transaction) => (
+        {transactions.map((transaction) => (
           <Transaction key={transaction.id} transaction={transaction} />
-        )) }
+        ))}
       </ul>
     </>
-  )
-}
+  );
+};
